9aa608e4af9223da5a2cce48ce0d98ea35305902
ckc6776 <a5530469@yahoo.com.tw>
2023-05-22 19:36:46 +0800

    0522homework

--- WindowsFormsApp1/WindowsFormsApp1/Form_homework3.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form_homework3.Designer.cs
@@ -35,64 +35,83 @@ namespace WindowsFormsApp1
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(209, 191);
+            this.button1.Location = new System.Drawing.Point(395, 191);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "開始";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(323, 191);
+            this.button2.Location = new System.Drawing.Point(530, 191);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "暫停";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(444, 191);
+            this.button3.Location = new System.Drawing.Point(662, 191);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "歸零";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(299, 91);
+            this.label1.Location = new System.Drawing.Point(118, 91);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Text = "自動";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "未啟動";
             // 
             // Form_homework3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form_homework3";
             this.Text = "Form_homework3";
+            this.Load += new System.EventHandler(this.Form_homework3_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,5 +124,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Form_homework3.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form_homework3.cs
@@ -13,16 +13,19 @@ namespace WindowsFormsApp1
     public partial class Form_homework3 : Form
     {
         int count1 = 0;
-        
+        int count2 = 0;
+
+
         public Form_homework3()
         {
             InitializeComponent();
             timer1.Start();
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Text = String.Format("{0}秒",count1.ToString());
+            label1.Text = String.Format("{0}秒",count1++.ToString());
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -30,22 +33,36 @@ namespace WindowsFormsApp1
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+        private void Form_homework3_Load(object sender, EventArgs e)
         {
-            timer1.Enabled = true;
+
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void timer2_Tick(object sender, EventArgs e)
         {
-            timer1.Enabled = false;
+            label2.Text = String.Format("{0}秒", count2++.ToString());
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            timer1.Enabled = false;
-            count1 = 0;
-            label1.Text = "{0}秒";
-
+            timer2.Start();
+            timer2.Enabled = true;
+            label2.Text = String.Format("{0}秒", count2++.ToString());
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            label2.Text = String.Format("{0}秒", count2++.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            count2 = 0;
+            label2.Text = String.Format("{0}秒", count2++.ToString());
+        }
+
     }
 }
